aeab810937f824cf9406b452cbda0b47455cea0e
Majdouline01 <majfoline@gmail.com>
2023-04-01 23:13:31 +0200

    ;;

--- src/main/java/login/LogoutServlet.java
+++ src/main/java/login/LogoutServlet.java
@@ -11,6 +11,8 @@ public class LogoutServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 			req.getSession().invalidate();
 			res.sendRedirect("Login.jsp");
-		
+		   
+			
+			
 	}
 }
